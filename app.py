b088dcd785db0f92e781a45b902766059405d2f4
Ilikeforks3721 <157530210+Ilikeforks3721@users.noreply.github.com>
2026-04-02 16:40:57 +0100

    Moved some functions
    Removed "addTransport" and "points_calculator", and instead moved them to functions.py (for modularization purposes)

--- app.py
+++ app.py
@@ -1,23 +1,10 @@
 from datetime import datetime
-
 from flask import Flask, request, url_for, render_template, redirect, session, flash
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.testing.pickleable import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from models import transportList, User, TypesOfTransport, app, db, car_carbon_per_km, plane_long_range_carbon_per_km
-
-def addTransport(transportList):
-    addedCount = 0
-    for i in transportList:
-        exists = TypesOfTransport.query.filter_by(transport=i['transport']).first()
-        if not exists:
-            newTransport = TypesOfTransport(transport=i['transport'], carbonUse=i['carbonUse'])
-            db.session.add(newTransport)
-            addedCount+=1
-            print("Adding transport")
-    db.session.commit()
-    print("Added transport")
-    return addedCount
+from functions import addTransport, points_calculator
 
 with app.app_context():
     db.create_all()
@@ -25,18 +12,6 @@ with app.app_context():
         print("no transport found")
         addTransport()
         print(TypesOfTransport.query.count())
-
-def points_calculator(carbon_used, distance):
-    carbon_used_per_km = carbon_used/distance
-    standard_max_carbon = car_carbon_per_km
-    if distance > 1500:
-        standard_max_carbon = plane_long_range_carbon_per_km
-    elif distance > 750:
-        standard_max_carbon = (distance/750 - 1) * plane_long_range_carbon_per_km + (2 - distance/750) * car_carbon_per_km
-    carbon_saved_per_km = standard_max_carbon - carbon_used_per_km
-    carbon_saved = carbon_saved_per_km * distance
-    points = int(7.5 * carbon_saved)
-    return max(points, 0)
 
 @app.route('/')
 def homepage():
