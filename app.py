8fca3419af53e61c2c55d5b52cac59d841482b3d
Ilikeforks3721 <157530210+Ilikeforks3721@users.noreply.github.com>
2026-04-17 17:10:13 +0100

    Refactored and moved a function
    -Refactored some names for PEP8
    -Moved function get_points_by_day to functions.py for modularization purposes

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from datetime import datetime
-from datetime import timedelta
 from flask import request
 from flask import url_for
 from flask import render_template
@@ -14,13 +13,15 @@ from models import TypesOfTransport
 from models import app
 from models import db
 from models import PointsHistory
-from functions import addTransport, pointsCalculator
+from functions import add_transport
+from functions import points_calculator
+from functions import get_points_by_day
 
 with app.app_context():
     db.create_all()
     if TypesOfTransport.query.count() == 0:
-        print("no transport found")
-        addTransport()
+        print("No transport found")
+        add_transport()
         print(TypesOfTransport.query.count())
 
 @app.route('/')
@@ -42,88 +43,87 @@ def homepage():
         tree = 'small'
     elif 2500 < points <= 5000:
         tree = 'medium'
-    elif 5000 < points:
+    else:
         tree = 'large'
 
     return render_template('homepage.html',user=user, tree=tree)
 
 @app.route('/travel', methods=['GET', 'POST'])
-def travelLogging():
+def travel_logging():
     if 'userID' not in session:
         flash('Please sign in to log your journey', 'error')
         return redirect(url_for('login'))
 
     user = User.query.get(session['userID'])
 
-    totalCarbonUsage = None
-    transportName = None
-    distanceKM = None
-    transportTypes = TypesOfTransport.query.all()
+    total_carbon_usage = None
+    transport_name = None
+    distance_km = None
+    transport_types = TypesOfTransport.query.all()
 
-    if len(transportTypes) == 0:
-        addTransport()
-        transportTypes = TypesOfTransport.query.all()
+    if len(transport_types) == 0:
+        add_transport()
+        transport_types = TypesOfTransport.query.all()
 
     if request.method == 'POST':
-        journeyAction = request.form.get('journeyAction')
-        print(journeyAction)
+        journey_action = request.form.get('journeyAction')
+        print(journey_action)
 
-        if journeyAction == 'Add':
+        if journey_action == 'Add':
             try:
-                transportType = request.form.get('transportType')
+                transport_type = request.form.get('transportType')
                 distance_str = request.form.get('distance')
-                milesKM = request.form.get('milesKM')
+                miles_or_km = request.form.get('milesKM')
 
-                if not transportType or not distance_str:
+                if not transport_type or not distance_str:
                     flash('Fields cannot be left empty.', 'error')
 
                 else:
                     distance = float(distance_str)
 
-                    if milesKM == 'miles':
-                        distanceKM = distance * 1.609344
+                    if miles_or_km == 'miles':
+                        distance_km = distance * 1.609344
                     else:
-                        distanceKM = distance
+                        distance_km = distance
 
-                    transport = TypesOfTransport.query.get(transportType)
-
+                    transport = TypesOfTransport.query.get(transport_type)
                     if transport:
-                        totalCarbonUsage = distanceKM * transport.carbonUse
-                        transportName = transport.transport
+                        total_carbon_usage = distance_km * transport.carbonUse
+                        transport_name = transport.transport
 
                         if 'currentJourney' not in session:
                             session['currentJourney'] = []
 
-                        totalCarbonUsageRounded = f"{totalCarbonUsage:.3f}"
+                        total_carbon_usage_rounded = f"{total_carbon_usage:.3f}"
 
                         session['currentJourney'].append({
-                            'transport': transportName,
-                            'distance': distanceKM,
-                            'carbonUse': totalCarbonUsage,
-                            "carbonUseRounded": totalCarbonUsageRounded,
+                            'transport': transport_name,
+                            'distance': distance_km,
+                            'carbonUse': total_carbon_usage,
+                            "carbonUseRounded": total_carbon_usage_rounded,
                             'date':datetime.now().strftime('%Y-%m-%d %H:%M:%S')
                         })
                         session.modified = True
-                        flash(f"Travel segment added: {transportName} for {distanceKM} km.")
+                        flash(f"Travel segment added: {transport_name} for {distance_km} km.")
             except Exception as e:
                 flash(f'Error logging journey {str(e)}', 'error')
 
-        elif journeyAction == 'Submit Journey':
-            fullJourney = session.pop('currentJourney', [])
-            if not fullJourney:
+        elif journey_action == 'Submit Journey':
+            full_journey = session.pop('currentJourney', [])
+            if not full_journey:
                 flash('No travel added!', 'error')
             else:
-                totalCarbonUsage = sum(seg['carbonUse']for seg in fullJourney)
-                totalDistance = sum(seg['distance']for seg in fullJourney)
-                points = pointsCalculator(totalCarbonUsage, totalDistance)
+                total_carbon_usage = sum(seg['carbonUse']for seg in full_journey)
+                total_distance = sum(seg['distance']for seg in full_journey)
+                points = points_calculator(total_carbon_usage, total_distance)
 
                 if 'journeys' not in session:
                     session['journeys'] = []
 
                 session['journeys'].append({
-                    'transport': transportName,
-                    'distance': distanceKM,
-                    'carbonUse': totalCarbonUsage,
+                    'transport': transport_name,
+                    'distance': distance_km,
+                    'carbonUse': total_carbon_usage,
                     'points': points,
                     'date':datetime.now().strftime('%Y-%m-%d %H:%M:%S')
                 })
@@ -133,46 +133,24 @@ def travelLogging():
                 new_history = PointsHistory(user_id=user.id, points=points)
                 db.session.add(new_history)
                 db.session.commit()
-                flash(f"Journey logged successfully! You used {totalCarbonUsage:.3f} kg of C02", 'success')
+                flash(f"Journey logged successfully! You used {total_carbon_usage:.3f} kg of C02", 'success')
 
-        elif journeyAction == "Return Home":
+        elif journey_action == "Return Home":
             return redirect(url_for('homepage'))
 
-    return render_template('travelLogging.html', user=user, transportTypes=transportTypes, totalCarbonUsage=totalCarbonUsage, transportName=transportName, distance=distanceKM, currentJourney=session.get('currentJourney', []))
+    return render_template('travelLogging.html', user=user, transportTypes=transport_types, totalCarbonUsage=total_carbon_usage, transportName=transport_name, distance=distance_km, currentJourney=session.get('currentJourney', []))
 
 @app.route('/track_progress', methods=['GET', 'POST'])
-def progressTrack():
+def progress_track():
     if 'userID' not in session:
         flash('Please sign in to track your progress', 'error')
         return redirect(url_for('login'))
     
     user = User.query.get(session['userID'])
-    now = datetime.now()
 
     days = int(request.form.get('days', 7))
 
-    def getPointsByDay(days):
-        start_day = now - timedelta(days=days)
-        history = PointsHistory.query.filter(
-            PointsHistory.user_id == user.id,
-            PointsHistory.date >= start_day
-        ).all()
-
-        points_by_day = {}
-        for entry in history:
-            day = entry.date.strftime('%Y-%m-%d')
-            points_by_day[day] =  points_by_day.get(day, 0) + entry.points
-
-        labels = []
-        values = []
-        for i in range(days):
-            day = (start_day + timedelta(days=i+1)).strftime('%Y-%m-%d')
-            labels.append(day)
-            values.append(points_by_day.get(day, 0))
-
-        return labels, values
-
-    labels, values = getPointsByDay(days)
+    labels, values = get_points_by_day(days, PointsHistory, user)
     print("DAYS:", days)
     print("LABELS:", labels)
     print("VALUES:", values)
@@ -214,15 +192,15 @@ def register():
             flash("Passwords are not identical. Please try registering again.", "error")
             return render_template('register.html')
         
-        hashedPassword = generate_password_hash(password)
+        hashed_password = generate_password_hash(password)
 
-        existingUser = User.query.filter((User.username == username)|(User.email == email)).first()
-        if existingUser:
+        existing_user = User.query.filter((User.username == username)|(User.email == email)).first()
+        if existing_user:
             flash("Username/email already exists.", 'error')
             return render_template('register.html')
-        newUser = User(username = username, email = email, password = hashedPassword)
+        new_user = User(username = username, email = email, password = hashed_password)
         try:
-            db.session.add(newUser)
+            db.session.add(new_user)
             db.session.commit()
             flash(f'Welcome {username}. You have been successfully registered! Please login to start using ZeroHero.', 'success')
             return redirect(url_for('login'))
@@ -237,10 +215,10 @@ def login():
     if 'attempts' not in session:
         session['attempts'] = 0
     if request.method == 'POST':
-        usernameEmail = request.form['usernameEmail']
+        username_email = request.form['usernameEmail']
         password = request.form['password']
 
-        user = User.query.filter((User.username == usernameEmail)|(User.email == usernameEmail)).first()
+        user = User.query.filter((User.username == username_email)|(User.email == username_email)).first()
         if session['attempts']>=5:
             flash("Too many failed attempts!",'error')
         if user and check_password_hash(user.password, password):
